305ea8fd65a93eac15ba82f597ff913b5eabddd8
Tanvi Hegde <htanviii1210@gmail.com>
2026-03-13 18:54:40 +0530

    fix deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import path from "path";
 
 import { connectDB } from "./config/db.js";
 import notesRoutes from "./routes/notesRoutes.js";
@@ -11,29 +10,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve();
 
 app.use(express.json());
-
-if (process.env.NODE_ENV !== "production") {
-app.use(
-cors({
-origin: "http://localhost:5173",
-})
-);
-}
+app.use(cors());
 
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
-
-if (process.env.NODE_ENV === "production") {
-app.use(express.static(path.join(__dirname, "client", "dist")));
-
-app.get("*", (req, res) => {
-res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
-}
 
 connectDB()
 .then(() => {
